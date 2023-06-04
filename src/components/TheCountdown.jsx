6689b0ae7b753c1ea023e13bce2c476a9586c1be
kafkaesc <jared.a.hettinger@gmail.com>
2023-06-04 12:47:06 -0500

    Adding TheCountdown component and displaying it on home page

---
+++ src/components/TheCountdown.jsx
@@ -0,0 +1,20 @@
+import Button from '../elements/Button';
+import H2 from '../elements/H2';
+import HookBox from './HookBox';
+import { useCountdown } from '../hooks/useCountdown';
+
+export default function TheCountdown() {
+	const { theCountdown, decrementTheCountdown } = useCountdown(10);
+	return (
+		<HookBox>
+			<H2>Countdown</H2>
+			<div className="w-4 inline-block text-center">{theCountdown}</div>
+			<Button
+				aria-label="Decrement the countdown"
+				onClick={decrementTheCountdown}
+			>
+				-
+			</Button>
+		</HookBox>
+	);
+}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,11 @@
 import H1 from '../elements/H1';
+import TheCountdown from '../components/TheCountdown';
 
 export default function Home() {
 	return (
 		<>
 			<H1>Home Component</H1>
+			<TheCountdown />
 		</>
 	);
 }
