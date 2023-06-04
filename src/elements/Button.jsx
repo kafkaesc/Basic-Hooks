9d535081dc2c73051b0c71f0e1a0da78ab3e8a9b
kafkaesc <jared.a.hettinger@gmail.com>
2023-06-04 12:46:18 -0500

    Updating Button component

--- src/elements/Button.jsx
+++ src/elements/Button.jsx
@@ -2,7 +2,7 @@ export default function Button({ children, ...props }) {
 	return (
 		<button
 			{...props}
-			className="px-3 py-1 border border-black rounded text-black hover:border-black hover:bg-zinc-200"
+			className="px-2 border border-black bg-white rounded text-black hover:border-black hover:bg-zinc-200"
 		>
 			{children}
 		</button>
