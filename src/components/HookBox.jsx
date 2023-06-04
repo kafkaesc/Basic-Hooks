1a537e61f961b040ec345f187f63fb9756d5ba54
kafkaesc <jared.a.hettinger@gmail.com>
2023-06-04 12:58:39 -0500

    Adding bottom margin to HookBox

--- src/components/HookBox.jsx
+++ src/components/HookBox.jsx
@@ -1,3 +1,3 @@
 export default function HookBox({ children }) {
-	return <div className="p-2 rounded bg-slate-200">{children}</div>;
+	return <div className="p-2 mb-2 rounded bg-slate-200">{children}</div>;
 }
