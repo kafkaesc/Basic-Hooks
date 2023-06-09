3fb3ada51c5c9453677649d992217bdf41def716
kafkaesc <jared.a.hettinger@gmail.com>
2023-06-09 15:20:44 -0500

    Updating Home and About pages

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,5 +1,29 @@
+import A from '../elements/A';
+import Code from '../elements/Code';
 import H1 from '../elements/H1';
+import Link from '../elements/Link';
+import P from '../elements/P';
 
 export default function About() {
-	return <H1>About Component</H1>;
+	return (
+		<>
+			<H1>About Basic Hooks</H1>
+			<P className="text-center">Built by Jared Hettinger</P>
+			<P>
+				This project is an example of some basic custom hooks in React. The main
+				page will use two components that demonstrate the{' '}
+				<Code>useCountdown</Code> hook and the <Code>useCountDownUp</Code> hook.
+			</P>
+			<P>
+				The full code for this project is available at{' '}
+				<A href="https://github.com/kafkaesc/Basic-Hooks">
+					https://github.com/kafkaesc/Basic-Hooks
+				</A>
+				.
+			</P>
+			<div className="text-center">
+				<Link to="/">Home</Link>
+			</div>
+		</>
+	);
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,5 @@
 import H1 from '../elements/H1';
+import Link from '../elements/Link';
 import CountDownThenUp from '../components/CountDownThenUp';
 import TheCountdown from '../components/TheCountdown';
 
@@ -6,6 +7,9 @@ export default function Home() {
 	return (
 		<>
 			<H1>Home Component</H1>
+			<div className="text-center">
+				<Link to="/About">About</Link>
+			</div>
 			<TheCountdown />
 			<CountDownThenUp />
 		</>
